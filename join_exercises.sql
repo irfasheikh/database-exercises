cc6d72358f0591b251c49f4991cbd285a169c7e1
Irfa Sheikh <irfasheikh@gmail.com>
2021-05-25 21:57:33 -0500

    commit

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,3 +1,13 @@
 USE join_test_db;
 
---write a query
+--
+SELECT dpt.dept_name AS 'Department Name', CONCAT( e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees AS e
+         JOIN dept_manager AS dm
+              ON e.emp_no = dm.emp_no
+         JOIN departments AS dpt
+              ON dm.dept_no = dpt.dept_no
+WHERE to_date > CURRENT_DATE;
+
+
+SELECT
