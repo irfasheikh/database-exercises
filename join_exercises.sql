170967a02686da714022002ea1ee0f5ccfb7e821
Irfa Sheikh <irfasheikh@gmail.com>
2021-05-27 23:58:23 -0500

    cont work

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,6 +1,6 @@
 USE join_test_db;
 
---
+--write a query that shows each department along with the name of the current manager for that department.
 SELECT dpt.dept_name AS 'Department Name', CONCAT( e.first_name, ' ', e.last_name) AS 'Department Manager'
 FROM employees AS e
          JOIN dept_manager AS dm
@@ -9,5 +9,5 @@ FROM employees AS e
               ON dm.dept_no = dpt.dept_no
 WHERE to_date > CURRENT_DATE;
 
-
+--Find the name of all departments currently managed by women.
 SELECT
