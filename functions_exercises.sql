ca4a2ef8d906a99bab39efeb14a2447d1f59703d
Irfa Sheikh <irfasheikh@gmail.com>
2021-06-02 23:29:18 -0500

    continued work

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -2,3 +2,10 @@
 
 SELECT last_name FROM employees WHERE last_name LIKE 'E%'
 OR WHERE last_name LIKE '%E'
+
+   -- Find all employees born on Christmas
+
+   SELECT * FROM employees WHERE month(birth_date) =12 and day(birth_date) = 25;
+
+SELECT * FROM employees
+WHERE year(hire_date)
